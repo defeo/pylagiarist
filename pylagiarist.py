ff375ee01707fd50e4b5880cf08527af2f629ee1
Luca De Feo <defeo@pippin>
2016-08-14 23:05:32 +0200

    Ported to Python 3

--- pylagiarist.py
+++ pylagiarist.py
@@ -38,12 +38,12 @@ for path in args:
         exit('%s: Cannot open %s' % (os.path.basename(sys.argv[0]), path))
 
 
-filelist = filter(lambda f : 
-                  (not options.include or
-                   any(re.search(p, f) for p in options.include)) and
-                  (not options.exclude or
-                   not any(re.search(p, f) for p in options.exclude)),
-                  filelist)
+filelist = list(filter(lambda f : 
+                    (not options.include or
+                    any(re.search(p, f) for p in options.include)) and
+                    (not options.exclude or
+                    not any(re.search(p, f) for p in options.exclude)),
+                    filelist))
 
 if options.verbose:
     sys.stderr.write('Comparing %d files in %s ...\n' % (len(filelist), ', '.join(args)))
@@ -53,23 +53,23 @@ itotal = 2.0 / ((len(filelist)-1)*len(filelist))
 e = 1 + (len(filelist) > 50)
 while filelist:
     a = filelist.pop()
-    atext = open(a).read()
+    atext = open(a, 'rb').read()
     for b in filelist:
         old = round(count, e)
         count += itotal
         if options.verbose and old != round(count, e):
             sys.stderr.write('%d%%\r' % int(old*100))
 
-        btext = open(b).read()
+        btext = open(b, 'rb').read()
         sm1 = SequenceMatcher(a=atext, b=btext)
         sm2 = SequenceMatcher(a=btext, b=atext)
         if any((sm.real_quick_ratio() >= options.threshold
                 and sm.quick_ratio() >= options.threshold
                 and sm.ratio() >= options.threshold)
                for sm in (sm1, sm2)):
-            print '''
+            print('''
 Possible plagiarism: difflib %f, %f%s.
     %s
     %s''' % (sm1.ratio(), sm2.ratio(),
              ', Levenshtein %f' % lev.ratio(a,b) if lev else '',
-             a, b)
+             a, b))
